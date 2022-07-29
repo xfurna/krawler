7e98d57c768c980432475029b9f253eead0e73d0
xfurna <architdwivedi.off@gmail.com>
2022-07-29 18:28:04 +0530

    update: make things modular

---
+++ params.py
@@ -0,0 +1,7 @@
+BASE = 'https://undostres.com.mx/'
+FIND = {'https://tnc.undostres.com.mx/','https://tnc.undostres.com.mx/diavipfebrero2022/'}
+SKIP = ['mailto:','google.','#','care@','huawei','medium.com','twitter.','apple.','itune.','facebook.','whatsapp.','mx/index.php','instagram.','linkedin.','uber.',
+'youtube.', '.link/ansH']
+headers = {
+    'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
+}
